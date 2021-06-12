a1a56a828667ef236badc73fcc92f454e02473c6
anjie cao <anjiecao@stanford.edu>
2021-06-12 08:52:01 -0700

    clean up junk; some packaging and it still works, yay

---
+++ helper/get_kl_eig.R
@@ -0,0 +1,199 @@
+
+
+
+##########get_kl#############
+
+get_kl <- function (x,y) {
+  sum(x * log(x / y)) 
+}
+
+
+
+
+#########get_possible_creatures#########
+
+get_possible_creatures <- function(current_observation){
+  
+  flip_observation <- as.logical(1 - (current_observation) %>% 
+                                   as.logical()) %>% 
+    as.vector() %>% 
+    as_tibble_row(.name_repair = ~ names(current_observation)) 
+  
+  combine_observations <- bind_rows(current_observation, flip_observation)
+  
+  all_possible_creatures <- combine_observations %>% 
+    cross_df() %>% 
+    mutate(index = row_number())
+  
+  # will return a df of possible creatures 
+  return (all_possible_creatures) 
+}    
+
+
+#########get_possible_kls#########
+
+get_possible_kls <- function(
+  observations, 
+  all_possible_outcomes, 
+  posterior_at_t, 
+  grid_theta = grid_theta, 
+  grid_epsilon = grid_epsilon, 
+  alpha_prior = alpha_prior, 
+  beta_prior = beta_prior,
+  alpha_epsilon = alpha_epsilon, 
+  beta_epsilon = beta_epsilon){
+  
+  all_possible_outcomes <- all_possible_outcomes %>% 
+    mutate(index = row_number())
+  
+  possible_kls <- tibble(
+    "index" = all_possible_outcomes$index, 
+    "kl" = rep(NA, all_possible_outcomes$index %>% length())
+  )
+  for (i in 1: nrow(all_possible_outcomes)){
+    
+    current_scenario <- observations %>% 
+      bind_rows(all_possible_outcomes %>% 
+                  filter(index == i))
+    
+    posterior_for_current_scenario <- grid_apprxoimation_with_observation(
+      noisy_observation = current_scenario, 
+      track_epsilon = TRUE, 
+      grid_theta = grid_theta, 
+      grid_epsilon = grid_epsilon, 
+      alpha_prior = alpha_prior, 
+      beta_prior = beta_prior,
+      alpha_epsilon = alpha_epsilon, 
+      beta_epsilon = beta_epsilon
+    )
+    
+    
+    # now calculate kl feature by feature 
+    features <- posterior_for_current_scenario %>% 
+      distinct(feature_index) %>% 
+      pull()
+    
+    creature_kl <- get_kl(posterior_for_current_scenario$posterior, 
+                       posterior_at_t$posterior)
+    
+    
+    # bind creature kl with other kl 
+    
+    possible_kls$kl[i] <- creature_kl
+    
+    
+  }
+  
+  
+  # will return a df of possible kls 
+  return (possible_kls)  
+}
+
+
+#########noisy_post_pred for single creature#########
+
+noisy_post_pred <- function(theta, epsilon, posterior, heads = TRUE) {
+  p_1 <- sum(((1 - epsilon) * theta * posterior) + 
+               (epsilon * (1-theta) * posterior))
+  
+  ifelse(heads, p_1, 1 - p_1)
+  
+}
+
+#########noisy_post_pred for entire creature#########
+
+
+creature_noisy_post_pred <- function(
+  outcome_index, 
+  all_possible_outcomes, 
+  posterior_at_t){
+  
+  observation_at_t_plus_one <- all_possible_outcomes %>% 
+    filter(index == outcome_index)
+  
+  feature_n <- observation_at_t_plus_one %>% 
+    select(starts_with("V")) %>% 
+    ncol()
+  
+  
+  
+  # calculate post predctive for each feature
+  feature_predictive <- lapply(seq(1, feature_n, 1), 
+                               function(x,
+                                        observation = observation_at_t_plus_one,
+                                        posterior = posterior_at_t){
+                                 
+                                 f_posterior <- posterior %>% 
+                                   filter(feature_index == x)
+                                 
+                                 f_observation <- observation[x] %>% 
+                                   pull()
+                                 
+                                 noisy_post_pred(f_posterior$theta, 
+                                                 f_posterior$epsilon, 
+                                                 f_posterior$posterior, 
+                                                 f_observation)
+                                 
+                               }) 
+  
+  return(feature_predictive %>% unlist() %>% prod())
+  
+  
+}
+
+
+###########get_all_possible_creature_pred###########
+
+get_all_possible_creature_pred <- function(
+  all_possible_outcomes, 
+  posterior_at_t
+){
+  
+  
+  all_preds <- lapply(all_possible_outcomes$index, 
+                      function(x, 
+                               apo = all_possible_outcomes, 
+                               pt = posterior_at_t){
+                        
+                        creature_noisy_post_pred(x, apo, pt)
+                        
+                        
+                      }) %>% unlist()
+  
+  return(all_preds %>% unlist())
+  
+}
+
+
+#########get_eig#########
+
+get_eig <- function(current_observation, 
+                    observations, 
+                    posterior_at_t, 
+                    grid_theta = grid_theta, 
+                    grid_epsilon = grid_epsilon, 
+                    alpha_prior = alpha_prior, 
+                    beta_prior = beta_prior,
+                    alpha_epsilon = alpha_epsilon, 
+                    beta_epsilon = beta_epsilon){
+  
+  all_possible_outcomes <- get_possible_creatures(current_observation)
+  all_possible_kls <- get_possible_kls(
+    observations, 
+    all_possible_outcomes, 
+    posterior_at_t, 
+    grid_theta = grid_theta, 
+    grid_epsilon = grid_epsilon, 
+    alpha_prior = alpha_prior, 
+    beta_prior = beta_prior,
+    alpha_epsilon = alpha_epsilon, 
+    beta_epsilon = beta_epsilon)
+  
+  all_predictives <- get_all_possible_creature_pred(
+    all_possible_outcomes, 
+    posterior_at_t)
+  
+  return (all_possible_kls$kl %*% all_predictives)
+  
+  
+}
--- helper/grid_approximation.R
+++ helper/grid_approximation.R
@@ -1,144 +1,4 @@
-## -------------------------------------------------------------
-## grid with theta
-
-grid_with_theta <- function(
-  grid_theta = seq(0.01, .99, .01), 
-  epsilon = epsilon, 
-  noisy_observation, 
-  alpha_prior = 1, 
-  beta_prior = 1,
-  alpha_epsilon, 
-  beta_epsilon
-){
-  # special case this is for when only update based on 1 observation
-  if(!is.matrix(noisy_observation)){
-    feature_number = length(noisy_observation)
-    
-    lapply(seq(1, feature_number, 1), 
-           function(x){
-             update_grid_with_theta(
-               feature_index = x, 
-               thetas = grid_theta, 
-               z_bar = noisy_observation[x], 
-               epsilon = epsilon, 
-               alpha_theta = alpha_prior, 
-               beta_theta = beta_prior,
-               alpha_epsilon = alpha_epsilon, 
-               beta_epsilon = beta_epsilon
-             )
-           }
-    ) %>% 
-      bind_rows()
-  }else{
-    feature_number = ncol(noisy_observation)
-    
-    lapply(seq(1, feature_number, 1), 
-           function(x){
-             update_grid_with_theta(
-               feature_index = x, 
-               thetas = grid_theta, 
-               z_bar = noisy_observation[,x], 
-               epsilon = epsilon, 
-               alpha_theta = alpha_prior, 
-               beta_theta = beta_prior,
-               alpha_epsilon = alpha_epsilon, 
-               beta_epsilon = beta_epsilon
-             )
-           }
-    ) %>% 
-      bind_rows()
-  }
-}
-
-## -------------------------------------------------------------
-## grid with theta and epsilon (not integrating out epsilon
-
-grid_with_theta_and_epsilon <- function(
-  grid_theta = seq(0.01, .99, .01), 
-  grid_epsilon = seq(0.01, .99, .01), 
-  noisy_observation, 
-  alpha_prior = 1, 
-  beta_prior = 1,
-  alpha_epsilon, 
-  beta_epsilon
-){
-  # special case this is for when only update based on 1 observation
-  
-  if(!is.matrix(noisy_observation)){
-    feature_number = length(noisy_observation)
-    
-    lapply(seq(1, feature_number, 1), 
-           function(x){
-             update_grid_with_theta_and_epsilon(
-               feature_i = x, 
-               grid_theta = grid_theta, 
-               grid_epsilon = grid_epsilon, 
-               observations = noisy_observation[x], 
-               alpha_theta = alpha_prior, 
-               beta_theta = beta_prior,
-               alpha_epsilon = alpha_epsilon, 
-               beta_epsilon = beta_epsilon
-             )
-           }
-    ) %>% 
-      bind_rows()
-    
-  }else{
-    feature_number = ncol(noisy_observation)
-    
-    lapply(seq(1, feature_number, 1), 
-           function(x){
-             update_grid_with_theta_and_epsilon(
-               feature_i = x, 
-               grid_theta = grid_theta, 
-               grid_epsilon = grid_epsilon, 
-               observations = noisy_observation[,x], 
-               alpha_theta = alpha_prior, 
-               beta_theta = beta_prior,
-               alpha_epsilon = alpha_epsilon, 
-               beta_epsilon = beta_epsilon
-             )
-           }
-    ) %>% 
-      bind_rows()
-  }
-}
-
-
-
-
-# single feature 
-# for trial 1 when beta distribution hasn't been destroyed 
-
-update_grid_with_theta <- function(feature_index = 1, 
-                                   thetas = seq(0.01, .99, .02), 
-                                   z_bar, 
-                                   epsilon, 
-                                   alpha_theta, 
-                                   beta_theta,
-                                   alpha_epsilon, 
-                                   beta_epsilon){
-  
-  posterior_df <- tibble("theta" = thetas)
-  posterior_df$unnormalized_log_posterior <- sapply(thetas, 
-                                                    function(x){ 
-                                                      lp_theta_given_z(z_bar = z_bar, 
-                                                                       theta = x, 
-                                                                       epsilon = epsilon, 
-                                                                       alpha_theta = alpha_theta, 
-                                                                       beta_theta = beta_theta,
-                                                                       alpha_epsilon = alpha_epsilon, 
-                                                                       beta_epsilon = beta_epsilon)})
-  posterior_df$normalized_log_posterior <- posterior_df$unnormalized_log_posterior - matrixStats::logSumExp(posterior_df$unnormalized_log_posterior)
-  posterior_df$feature_index <- feature_index
-  
-  return(posterior_df)
-  
-}
-
-
-
-
+source(here("helper/noisy_update.R"))
 update_grid_with_theta_and_epsilon <- function(
   feature_i, 
   grid_theta, 
@@ -166,122 +26,56 @@ update_grid_with_theta_and_epsilon <- function(
   
   samps$log_posterior = samps$unnormalized_log_posterior - matrixStats::logSumExp(samps$unnormalized_log_posterior)
   
-  # integrate out epsilon by summing p(theta | z) over all the different possible values of epsilon
-  # that's the log-sum-exp line
+  
   theta_posterior <- samps %>%
-    group_by(theta) %>%
-    summarise(log_posterior = matrixStats::logSumExp(log_posterior)) %>%
     mutate(posterior = exp(log_posterior)) %>% 
     mutate(feature_index = feature_i)
+  
   
   return(theta_posterior)
   
 }
 
 
-
-
-
-
-grid_with_theta_and_epsilon_has_epsilon <- function(
+grid_apprxoimation_with_observation <- function(
+  noisy_observation, 
+  track_epsilon = TRUE, 
   grid_theta = seq(0.01, .99, .01), 
   grid_epsilon = seq(0.01, .99, .01), 
-  noisy_observation, 
   alpha_prior = 1, 
   beta_prior = 1,
-  alpha_epsilon, 
-  beta_epsilon
-){
-  # special case this is for when only update based on 1 observation
+  alpha_epsilon = 10, 
+  beta_epsilon = 1
   
-  # if(!is.matrix(noisy_observation)){
-  #   feature_number = length(noisy_observation)
-  #   
-  #   lapply(seq(1, feature_number, 1), 
-  #          function(x){
-  #            update_grid_with_theta_and_epsilon_has_epsilon(
-  #              feature_i = x, 
-  #              grid_theta = grid_theta, 
-  #              grid_epsilon = grid_epsilon, 
-  #              observations = noisy_observation[x], 
-  #              alpha_theta = alpha_prior, 
-  #              beta_theta = beta_prior,
-  #              alpha_epsilon = alpha_epsilon, 
-  #              beta_epsilon = beta_epsilon
-  #            )
-  #          }
-  #   ) %>% 
-  #     bind_rows()
-  #   
-  #   
-  #   
-  # }else{
-  #   
-    
-    feature_number = ncol(noisy_observation)
-    
-    lapply(seq(1, feature_number, 1), 
-           function(x){
-             update_grid_with_theta_and_epsilon_has_epsilon(
-               feature_i = x, 
-               grid_theta = grid_theta, 
-               grid_epsilon = grid_epsilon, 
-               observations = noisy_observation[,x], 
-               alpha_theta = alpha_prior, 
-               beta_theta = beta_prior,
-               alpha_epsilon = alpha_epsilon, 
-               beta_epsilon = beta_epsilon
-             )
-           }
-    ) %>% 
-      bind_rows()
-  
-  
-  
-  
-  
-  
-  
-}
-
-
-
-
-update_grid_with_theta_and_epsilon_has_epsilon <- function(
-  feature_i, 
-  grid_theta, 
-  grid_epsilon, 
-  observations, 
-  alpha_theta, beta_theta, 
-  alpha_epsilon, beta_epsilon
 ){
   
+  total_feature_number = ncol(noisy_observation %>% select(starts_with("V")))
   
-  samps <- expand_grid(theta = grid_theta,
-                       epsilon = grid_epsilon) 
+  posterior_df <- lapply(seq(1, total_feature_number, 1), 
+         function(x){
+           update_grid_with_theta_and_epsilon(
+             feature_i = x, 
+             grid_theta = grid_theta, 
+             grid_epsilon = grid_epsilon, 
+             observations = noisy_observation[,x], 
+             alpha_theta = alpha_prior, 
+             beta_theta = beta_prior,
+             alpha_epsilon = alpha_epsilon, 
+             beta_epsilon = beta_epsilon
+           )
+         }
+  ) %>% 
+    bind_rows()
   
-  
-  samps$unnormalized_log_posterior <- mapply(function(x, y) 
-    lp_theta_given_z(z_bar = observations, 
-                     theta = x, 
-                     epsilon = y, 
-                     alpha_theta = alpha_theta, 
-                     beta_theta = beta_theta,
-                     alpha_epsilon = alpha_epsilon, 
-                     beta_epsilon = beta_epsilon), 
-    samps$theta, 
-    samps$epsilon)
-  
-  samps$log_posterior = samps$unnormalized_log_posterior - matrixStats::logSumExp(samps$unnormalized_log_posterior)
-  
-  
-  theta_posterior <- samps %>%
-    mutate(posterior = exp(log_posterior)) %>% 
-    mutate(feature_index = feature_i)
-  
-  
-  return(theta_posterior)
-  
+  if (track_epsilon){
+    return (posterior_df)
+  }else{
+    return (
+      posterior_df <- posterior_df %>% 
+        group_by(theta) %>%
+        summarise(log_posterior = matrixStats::logSumExp(log_posterior)) %>%
+        mutate(posterior = exp(log_posterior)))
+  }
+
+
 }
-
-
