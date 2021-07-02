415d85c91be361e8266e3aa949323d2c6cc22d0c
anjie cao <anjiecao@stanford.edu>
2021-07-02 07:24:33 -0700

    made some progress and discovered a sad truth about how slow the core function is; maybe it won't matter too much because latter function can just access it

--- helper/main_simulation_under_construction.r
+++ helper/main_simulation_under_construction.r
@@ -14,21 +14,39 @@ main_simulation_uc <- function(subject = x,
                 optimize = FALSE){
   
   
-  # df for keep track of model behavior, 
+  feature_number <- ncol(stimuli_sequence[startsWith(names(stimuli_sequence), 
+                                                     "V")])
+  
+  # df for keeping track of model behavior, 
   #df_model <- 
   
   # df for keep track of actual observations 
-  #df_observation <- 
+  df_observation <- data.frame(matrix(ncol = ncol(stimuli_sequence), 
+                                      nrow = max_observation)) %>% 
+    tibble()  
   
-  # df for keep track of posterior distribution 
-  #df_posterior <- 
+  colnames(df_observation) <- colnames(stimuli_sequence)
+  
+  df_observation$t <- seq(1, max_observation, 1)
+  df_observation$trial_type <- rep(NA_character_, max_observation)
+  df_observation$trial_number <- rep(NA_integer_, max_observation)
   
   # df for all possible creatures 
   #based on the stimuli vector length generate permuations
   
-  # create a dictionary of dfs to keep track of ALL posterior distribution 
-  # this way we can always look up the posterior from previous observation  
-  #dict_df_posterior <- 
+  # create a list of dfs to keep track of ALL posterior distribution 
+  # actually may consider keeping track of everything in matrix for multi-feature version 
+  # this way we can always look up the posterior from previous observation 
+  
+  # df for keep track of posterior distribution 
+  df_posterior <- expand_grid(theta = grid_theta,
+                              epsilon = grid_epsilon,
+                              feature_index = seq(1, feature_number))
+  # not sure when do we really need the non-log one, save some $$$  
+  df_posterior$log_posterior <- NA_real_
+
+  list_df_posterior <- lapply(seq(1, max_observation, 1), 
+                              function(x){df_posterior})
   
   # stimulus_idx <- 1
   t <- 1
@@ -36,14 +54,37 @@ main_simulation_uc <- function(subject = x,
   
   while(stimulus_idx <= total_trial_number && t <= max_observation){
     
+    #get stimulus
+    current_stimulus <- stimuli_sequence[stimulus_idx,]
+    
     #get observation 
+    current_observation <- noisy_observation_creature(
+      stimuli_df = stimuli_sequence,
+      trial_index  = stimulus_idx, 
+      n_sample = 1, 
+      epsilon = noise_parameter
+    )
     
-    #update observation df 
-    #df_observation ...
+    # add to current observation 
+    df_observation[df_observation$t == t, str_detect(names(df_observation), "V")] <-
+      current_observation
+    df_observation[df_observation$t == t, "trial_type"] <- current_stimulus$trial_type
+    df_observation[df_observation$t == t, "trial_number"] <- stimulus_idx
     
+   
     #update posterior df 
-    #prev_posterior_df <- # accessing through dict_df_posterior 
+    if(t == 1){
+      # do some fresh calculation
+    }else{
+      prev_posterior_df <- list_df_posterior[[t-1]]
+    }
+    
+    # accessing through dict_df_posterior 
+      # if it is the first one, calculate lp_theta() and lp(epsilon)
+      # if not, just use the previous value 
     #curr_posterior_df <- # udpate using prev_posterior_df and observation 
+    
+    
     
     #update dict_df_posterior 
     #...
