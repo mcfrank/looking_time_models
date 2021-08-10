7dc92117ba146237dfa446935659a3e4ecde7a00
anjie cao <anjiecao@stanford.edu>
2021-08-09 21:18:49 -0700

    set up psuedo interface

--- helper/d_update_posterior.R
+++ helper/d_update_posterior.R
@@ -1,34 +1,45 @@
 
-get_df_lp_z_y_raw <- function(t, index, df_model, m_observation, current_observation, grid_epsilon, df_lp_y_given_theta){
+get_df_lp_z_y_raw <- function(t, stimulus_idx, last_t_for_last_stimulus, index, df_model, m_observation, current_observation, grid_epsilon, df_lp_y_given_theta){
   
-  current_stimulus_idx = df_model$stimulus_idx[[t]]
-  last_t_for_last_stimulus = ifelse(current_stimulus_idx == 1, 1,
-                                    max((df_model[df_model$stimulus_idx == current_stimulus_idx-1,])$t, na.rm = TRUE)
-                                    )
-  
+
   observations_on_this_stimulus_till_this_t = na.omit(m_observation[last_t_for_last_stimulus:t, index])
                                                      
-
   df_lp_z_given_y = tibble(
     "epsilon" = grid_epsilon
   )
 
-  df_lp_z_given_y$lp_z_given_y_ONE = ifelse(t == 1 || df_model$stimulus_idx[[t]] != df_model$stimulus_idx[[t-1]],
+  df_lp_z_given_y$lp_z_given_y_ONE = ifelse(t == 1 || stimulus_idx != df_model$stimulus_idx[[t-1]],
                                             lp_z_ij_given_y(zij = current_observation[[index]], yi = 1, epsilon = grid_epsilon), 
                                             rowSums(sapply(observations_on_this_stimulus_till_this_t, 
                                                            function(x){lp_z_ij_given_y(x, 1, grid_epsilon)}))
                                             )
-  df_lp_z_given_y$lp_z_given_y_ZERO = ifelse(t == 1 || df_model$stimulus_idx[[t]] != df_model$stimulus_idx[[t-1]],
+  df_lp_z_given_y$lp_z_given_y_ZERO = ifelse(t == 1 || stimulus_idx != df_model$stimulus_idx[[t-1]],
                                             lp_z_ij_given_y(zij = current_observation[[index]], yi = 0, epsilon = grid_epsilon), 
                                             rowSums(sapply(observations_on_this_stimulus_till_this_t, 
                                                            function(x){lp_z_ij_given_y(x, 0, grid_epsilon)}))
   )
-  
   df_lp_z_y_raw <- expand_grid(df_lp_y_given_theta, df_lp_z_given_y)
-  
-
 }
 
+
+get_lp_z_given_theta_for_raw <- function(t, last_t_for_last_stimulus, index, df_model, 
+                                         current_df_lp_z_given_theta, ll_df_z_given_theta){
+
+   if(t == 1 | df_model$stimulus_idx[[t]] == 1){
+     lp_z_given_theta <-rowLogSumExps(lx = as.matrix(current_df_lp_z_given_theta[,c("lp_z_y_ONE", "lp_z_y_ZERO")]))
+  }else{
+      if(df_model$stimulus_idx[[t]] != df_model$stimulus_idx[[t-1]]){
+     lp_z_given_theta <- rowLogSumExps(lx = as.matrix(current_df_lp_z_given_theta[,c("lp_z_y_ONE", "lp_z_y_ZERO")])) + 
+                               (ll_df_z_given_theta[[t-1]][[index]])$lp_z_given_theta
+                            }else{
+      lp_z_given_theta<- rowLogSumExps(lx = as.matrix(current_df_lp_z_given_theta[,c("lp_z_y_ONE", "lp_z_y_ZERO")])) + 
+                                (ll_df_z_given_theta[[last_t_for_last_stimulus]][[index]])$lp_z_given_theta
+                            }
+        }
+                             
+
+  
+}
 
 get_df_lp_z_given_theta <- function(t, 
                                     df_lp_y_given_theta, 
@@ -42,46 +53,18 @@ get_df_lp_z_given_theta <- function(t,
                                     alpha_theta, beta_theta, 
                                     alpha_epsilon, beta_epsilon){
   
-  current_df_lp_z_given_theta = ll_df_z_given_theta[[t]][[index]] 
-  df_lp_z_y_raw <-  get_df_lp_z_y_raw(t, index, df_model, m_observation, current_observation, grid_epsilon, df_lp_y_given_theta)
-  
-  # see if this is a new stimulus or an old stimulus 
-  # if starting a new stimulus 
-  # relying on short circuiting to handle the t=1 part
-  if(t == 1 || df_model$stimulus_idx[[t]] != df_model$stimulus_idx[[t-1]]){
-    
-    
-    
-    current_df_lp_z_given_theta$lp_z_y_ONE <-  df_lp_z_y_raw$lp_y_ONE_given_theta + df_lp_z_y_raw$lp_z_given_y_ONE
-    current_df_lp_z_given_theta$lp_z_y_ZERO <- df_lp_z_y_raw$lp_y_ZERO_given_theta + df_lp_z_y_raw$lp_z_given_y_ZERO
-    current_df_lp_z_given_theta$lp_z_given_theta <- ifelse(t == 1,rowLogSumExps(lx = as.matrix(current_df_lp_z_given_theta[,c("lp_z_y_ONE", "lp_z_y_ZERO")])), 
-                                                           rowLogSumExps(lx = as.matrix(current_df_lp_z_given_theta[,c("lp_z_y_ONE", "lp_z_y_ZERO")])) + 
-                                                             + (ll_df_z_given_theta[[t-1]][[index]])$lp_z_given_theta)  
-  
-  }else{
-    
-    # current only looking at single feature creature 
-    # OK THIS ACTUALLY NEEDS TO BE ALL OBSERVATIONS ON THIS STIMULUS 
-    current_stimulus_idx = df_model$stimulus_idx[[t]]
-    if (current_stimulus_idx == 1){
-      observations_on_this_stimulus_till_this_t = na.omit(m_observation[1:t, index])
-      
-    
-      
+  current_df_lp_z_given_theta = ll_df_z_given_theta[[t]][[index]]
+  last_t_for_last_stimulus = ifelse(stimulus_idx == 1, 1,
+                                    max((df_model[df_model$stimulus_idx == stimulus_idx-1,])$t, na.rm = TRUE)
+  )
 
-      current_df_lp_z_given_theta$lp_z_y_ONE <-  df_lp_z_y_raw$lp_y_ONE_given_theta + df_lp_z_y_raw$lp_z_given_y_ONE
-      current_df_lp_z_given_theta$lp_z_y_ZERO <- df_lp_z_y_raw$lp_y_ZERO_given_theta + df_lp_z_y_raw$lp_z_given_y_ZERO
-      current_df_lp_z_given_theta$lp_z_given_theta <- rowLogSumExps(lx = as.matrix(current_df_lp_z_given_theta[,c("lp_z_y_ONE", "lp_z_y_ZERO")]))
-    }else{
-      last_t_for_last_stimulus = max((df_model[df_model$stimulus_idx == current_stimulus_idx-1,])$t, na.rm = TRUE)
-      observations_on_this_stimulus_till_this_t = na.omit(m_observation[(last_t_for_last_stimulus+1):t, index])
-      
-
-      current_df_lp_z_given_theta$lp_z_y_ONE <-  df_lp_z_y_raw$lp_y_ONE_given_theta + df_lp_z_y_raw$lp_z_given_y_ONE
-      current_df_lp_z_given_theta$lp_z_y_ZERO <- df_lp_z_y_raw$lp_y_ZERO_given_theta + df_lp_z_y_raw$lp_z_given_y_ZERO
-      current_df_lp_z_given_theta$lp_z_given_theta <- rowLogSumExps(lx = as.matrix(current_df_lp_z_given_theta[,c("lp_z_y_ONE", "lp_z_y_ZERO")])) + 
-        (ll_df_z_given_theta[[last_t_for_last_stimulus]][[index]])$lp_z_given_theta
-    }
-  }
+  df_lp_z_y_raw <-  get_df_lp_z_y_raw(t, stimulus_idx, last_t_for_last_stimulus, index, df_model, m_observation, current_observation, grid_epsilon, df_lp_y_given_theta)
+  
+  current_df_lp_z_given_theta$lp_z_y_ONE <-  df_lp_z_y_raw$lp_y_ONE_given_theta + df_lp_z_y_raw$lp_z_given_y_ONE
+  current_df_lp_z_given_theta$lp_z_y_ZERO <- df_lp_z_y_raw$lp_y_ZERO_given_theta + df_lp_z_y_raw$lp_z_given_y_ZERO
+  current_df_lp_z_given_theta$lp_z_given_theta <- get_lp_z_given_theta_for_raw(t, last_t_for_last_stimulus, index, 
+                                                                               df_model, current_df_lp_z_given_theta, ll_df_z_given_theta)
+  
+ 
   return (current_df_lp_z_given_theta)
 }
