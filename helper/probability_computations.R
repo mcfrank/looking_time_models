029dad9ae56b040be07705c6e6c52c8dc906ac5e
Mike Frank <mcfrank@stanford.edu>
2021-11-04 14:31:46 -0700

    cleanup fom cowork

--- helper/probability_computations.R
+++ helper/probability_computations.R
@@ -5,7 +5,6 @@
 # - all functions have `score_` in their name and (for length) not `lp_`
 # - score functions return log probabilities
 # - no datatypes are in the names of the variables. 
-# - TODO: distinguish between a single instance and the full dataset? 
 #
 # This script contains all testable computations for the models.
 
@@ -23,10 +22,10 @@ score_z_given_theta <- function(t, # timestep
   this_lp_z_given_theta <- lp_z_given_theta[[t]][[f]]
   grid_epsilon <- unique(this_lp_z_given_theta$epsilon)
   grid_theta <- unique(this_lp_z_given_theta$theta)
-  stimulus_idx <- model$stimulus_idx[t]
+  this_stimulus_idx <- model$stimulus_idx[t]
   
   # need to compute over all noisy observations of this stimulus
-  observations_this_stimulus <- filter(model, stimulus_idx == stimulus_idx) %>%
+  observations_this_stimulus <- filter(model, stimulus_idx == this_stimulus_idx) %>%
     select(paste0("f", f)) %>%
     pull()
   
@@ -35,9 +34,9 @@ score_z_given_theta <- function(t, # timestep
   
   # compute probabilities over all observations of this stimulus
   lp_z_given_y$z_given_y_ONE = rowSums(sapply(observations_this_stimulus, 
-                                              function(x){ lp_z_ij_given_y(x, 1, grid_epsilon)}))
+                                              function(x){ score_z_ij_given_y(x, 1, grid_epsilon)}))
   lp_z_given_y$z_given_y_ZERO = rowSums(sapply(observations_this_stimulus, 
-                                               function(x){ lp_z_ij_given_y(x, 0, grid_epsilon)}))
+                                               function(x){ score_z_ij_given_y(x, 0, grid_epsilon)}))
   
   # clever expansion with cached likelihoods
   lp_z_y_theta <- expand_grid(lp_y_given_theta, lp_z_given_y)
@@ -51,7 +50,7 @@ score_z_given_theta <- function(t, # timestep
     rowLogSumExps(lx = as.matrix(this_lp_z_given_theta[,c("lp_z_y_ONE", "lp_z_y_ZERO")]))
   
   # add in likelihood for last sample from last stimulus, which includes all prior obs
-  if (stimulus_idx > 1) {
+  if (this_stimulus_idx > 1) {
     last_stim_last_t <- max(model$t[model$stimulus_idx == stimulus_idx - 1], na.rm=TRUE)
     this_lp_z_given_theta$lp_z_given_theta <- this_lp_z_given_theta$lp_z_given_theta + 
       lp_z_given_theta[[last_stim_last_t]][[f]]$lp_z_given_theta
@@ -66,8 +65,8 @@ score_z_given_theta <- function(t, # timestep
 score_post <- function(lp_z_given_theta, lp_prior, lp_post) {
   
   lp_post$unnormalized_log_posterior <- lp_z_given_theta$lp_z_given_theta + 
-    lp_theta_epsilon$lp_theta + 
-    lp_theta_epsilon$lp_epsilon
+    lp_prior$lp_theta + 
+    lp_prior$lp_epsilon
   lp_post$log_posterior <- lp_post$unnormalized_log_posterior - matrixStats::logSumExp(lp_post$unnormalized_log_posterior)
   lp_post$posterior <- exp(lp_post$log_posterior)
   
@@ -76,7 +75,7 @@ score_post <- function(lp_z_given_theta, lp_prior, lp_post) {
 
 # ---------------- score_z_ij_given_y ---------------------
 # p(z_ij | y)
-score_z_ij_given_y <- function(zij, yi, epsilon){
+score_z_ij_given_y <- function(zij, yi, epsilon) {
   
   if (zij == yi){
     log(1 - epsilon)
@@ -85,6 +84,7 @@ score_z_ij_given_y <- function(zij, yi, epsilon){
   }
 }
 
+
 # ---------------- score_prior ---------------------
 # p(z_ij | y)
 score_prior <- function(grid_theta, grid_epsilon, 
@@ -92,9 +92,9 @@ score_prior <- function(grid_theta, grid_epsilon,
                         alpha_epsilon, beta_epsilon) {
   
   thetas = tibble(theta = grid_theta, 
-                  lp_theta = lp_theta(grid_theta, alpha_prior, beta_prior))
+                  lp_theta = score_theta(grid_theta, alpha_prior, beta_prior))
   epsilons = tibble(epsilon = grid_epsilon, 
-                    lp_epsilon = lp_epsilon(grid_epsilon, alpha_epsilon, beta_epsilon))
+                    lp_epsilon = score_epsilon(grid_epsilon, alpha_epsilon, beta_epsilon))
   
   lp_theta_epsilon = expand_grid(thetas, epsilons)
   return(lp_theta_epsilon) 
@@ -161,14 +161,6 @@ score_z_ij_given_theta <- function(zij, theta, epsilon) {
   )
 }
 
-# ---------------- score_z_ij_given_y ---------------------
-score_z_ij_given_y <- function(zij, yi, epsilon){
-  if (zij == yi) {
-    log(1 - epsilon)
-  } else {
-    log(epsilon)
-  }
-}
 
 # ---------------- score_yi_given_theta ---------------------
 score_yi_given_theta <- function(yi, theta){
@@ -186,13 +178,7 @@ score_epsilon <- function(epsilon, alpha_epsilon, beta_epsilon){
   dbeta(x = epsilon, shape1 = alpha_epsilon, shape2 = beta_epsilon, log = TRUE)
 }
 
-
-
-
-
-
 # ---------------- compute KL divergence ---------------------
-# TODO: this could be replaced with something native? 
 # this is untested and has no input checking etc. 
 kl_div <- function (x, y) {
   sum(x * log(x / y)) 
@@ -200,7 +186,6 @@ kl_div <- function (x, y) {
 
 # ---------------- get_post_pred ---------------------
 # get posterior predictive
-# TODO: this is not done in log space and could have underflow errors!
 get_post_pred <- function(lp_post, heads = TRUE) {
   p_1 <- sum(((1 - lp_post$epsilon) * lp_post$theta * lp_post$posterior) + 
                (lp_post$epsilon * (1-lp_post$theta) * lp_post$posterior))
