a3a19a7dd1b034eda0d4f131fdcad34a841f017b
GalRaz <galraz@gmail.com>
2021-11-11 16:03:10 -0500

    some work on testing functions

--- helper/main_simulation_helper.R
+++ helper/main_simulation_helper.R
@@ -18,6 +18,8 @@ main_simulation <- function(params = df,
   # list of lists of df for the posteriors and likelihoods
   lp_post <- initialize_posterior(grid_theta, grid_epsilon, 
                                   params$max_observation, params$n_features)
+  
+  
   lp_z_given_theta <- initialize_z_given_theta(grid_theta, grid_epsilon, 
                                                   params$max_observation, 
                                                   params$n_features)
@@ -81,6 +83,9 @@ main_simulation <- function(params = df,
       lp_post[[t]][[f]] <- score_post(lp_z_given_theta = lp_z_given_theta[[t]][[f]], 
                                       lp_prior = lp_prior, 
                                       lp_post = lp_post[[t]][[f]])
+      
+      
+      browser()
     }
     
     # -compute new posterior grid over all possible outcomes
@@ -99,10 +104,14 @@ main_simulation <- function(params = df,
                               lp_z_given_theta = lp_z_given_theta,
                               model = model)
         
+        browser()
+        
         # upcoming posterior
         lp_post_new[[o]][[f]] <- score_post(lp_z_given_theta = lp_z_given_theta_new[[o]][[f]], 
                                             lp_prior = lp_prior, 
                                             lp_post = lp_post_new[[o]][[f]])
+        
+        
         
         # posterior predictive
         p_post_new[o,f] <- get_post_pred(lp_post[[t]][[f]], 
@@ -113,6 +122,7 @@ main_simulation <- function(params = df,
                               lp_post[[t]][[f]]$posterior)
       }
     }
+    
     
     # compute EIG
     if(KL_combo){
@@ -132,12 +142,11 @@ main_simulation <- function(params = df,
       stimulus_idx <- stimulus_idx + 1
     }
     
+    
     # update books
     t <- t+1
  
     } # FINISH HUGE WHILE LOOP
-  
-  browser()
   
   return(model)  
 }
