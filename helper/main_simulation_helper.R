dab7a206f45144dde8c7db9069cd07cc00c86594
GalRaz <galraz@gmail.com>
2021-12-14 18:05:47 -0500

    garbage commit

--- helper/main_simulation_helper.R
+++ helper/main_simulation_helper.R
@@ -126,12 +126,21 @@ main_simulation <- function(params = df,
         # kl between old and new posteriors
         kl_new[o,f] <- kl_div(lp_post_new[[o]][[f]]$posterior,
                               lp_post[[t]][[f]]$posterior)
+<<<<<<< HEAD
 
         #reset the model so that it doesn't behave weird in the end
         model[t+1, paste0("f", f)] <- NA
 
       }
     }
+=======
+ 
+      }
+    }
+    
+    
+    
+>>>>>>> parent of 818f38f... Merge pull request #21 from mcfrank/clean-up-main
 
     # reset the model stimulus so that it doesn't behave weird at the last stimulus
     model$stimulus_idx[t+1] <- NA_real_
