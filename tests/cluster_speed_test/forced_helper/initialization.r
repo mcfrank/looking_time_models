2303d9e9eb6bbc29ad5ac0600272086c8f819005
anjie cao <anjiecao@stanford.edu>
2022-03-14 17:29:00 -0700

    infrastructure for speed testing (why is the forced exp timing out?)

---
+++ tests/cluster_speed_test/forced_helper/initialization.r
@@ -0,0 +1,196 @@
+# ALL FUNCTIONS DEALING WITH INITIALIZING MODEL COMPUTATION
+
+
+# a list of dataframes that keep tracks of all the combinations possible 
+# index corresponds to trial number 
+# dataframes correspond to the possible concept1 vs concept2 combination
+initialize_two_concepts_combinations <- function(total_trial_number){
+  
+  lapply(1:total_trial_number, 
+         function(trial_number){
+           as.matrix(expand.grid(as.data.frame(sapply(1:trial_number, 
+                                                      FUN = function(i){c(1,2)}))))
+         })
+  
+}
+
+# a list of dataframe that keep tracks of all the y = 0 y = 1 combinations 
+# index corresponds to trial number 
+initialize_y_value_combinations <- function(total_trial_number){
+
+  lapply(1:total_trial_number, 
+         function(trial_number){
+           as.matrix(expand.grid(as.data.frame(sapply(1:trial_number, 
+                                                      FUN = function(i){c(0,1)}))))
+         })
+  
+}
+
+
+initialize_posterior <- function(grid_theta, grid_epsilon, max_observation, feature_number){
+  posterior <- expand_grid(theta = grid_theta,
+                              epsilon = grid_epsilon)
+  # not sure when do we really need the non-log one, save some $$$  
+  posterior$unnormalized_log_posterior <- NA_real_
+  posterior$log_posterior <- NA_real_
+  posterior$posterior <- NA_real_
+  
+  posterior <- lapply(seq(1, max_observation, 1), 
+                            function(x){
+                              lapply(seq(1, feature_number, 1), 
+                                     function(y){
+                                       posterior
+                                     })
+                            })
+  return(posterior)
+}
+
+initialize_z_given_theta <- function(grid_theta, grid_epsilon, max_observation, feature_number){
+  z_given_theta <- expand_grid(theta = grid_theta, 
+                                  epsilon = grid_epsilon)
+  
+  z_given_theta$lp_z_y_ONE <- NA_real_
+  z_given_theta$lp_z_y_ZERO <-  NA_real_
+  z_given_theta$lp_z_given_theta <- NA_real_
+
+  z_given_theta <- lapply(seq(1, max_observation, 1), 
+                                function(x){
+                                  lapply(seq(1, feature_number, 1), 
+                                         function(y){
+                                           z_given_theta
+                                         })
+                                })
+  return(z_given_theta)
+  
+}
+
+# model data frame, includes observations
+initialize_model <- function(eig_from_world, max_observation, n_features, measurement = "EIG") {
+  
+    model <- tibble(t = rep(NA,max_observation),
+                    stimulus_idx = rep(NA,max_observation), 
+                    EIG_from_world = rep(eig_from_world,max_observation),
+                    p_look_away = rep(NA,max_observation), 
+                    look_away = rep(NA,max_observation)) 
+    
+    if(measurement == "EIG"){
+      model$EIG = rep(NA,max_observation)
+    }else if(measurement == "KL"){
+      model$KL = rep(NA,max_observation)
+    }else if(measurement == "surprisal"){
+      model$surprisal = rep(NA,max_observation)
+    }
+  
+  # initialize columns for observations
+  model[,paste("f", 1:n_features, sep="")] <- NA
+  
+  return(model)
+}
+
+# data f for keep track of actual observations 
+initialize_m_observation <- function(feature_number, max_observation, stimuli_sequence){
+  
+  m_observation <- matrix(ncol = feature_number, 
+                          nrow = max_observation)
+  colnames(m_observation) <- grep("V",names(stimuli_sequence), value = TRUE)
+  return(m_observation)
+}
+
+
+# helper function that gets you a df with features as columns and all possible
+# combinations
+
+get_possible_observations_for_feature <- function(n_features){
+  matrix(rep(c(TRUE, FALSE), n_features), nrow = 2, byrow = FALSE)
+}
+
+get_possible_observations <- function(n_features) {
+  feature_list <- list()
+
+  for (i in 1:n_features) {
+    feature_list[[i]] <- c(FALSE, TRUE)
+  }
+
+  expand.grid(feature_list) %>%
+    tibble() %>%
+    rename_with(~gsub("Var", "V", .x)) # fix random naming thing
+}
+
+
+
+noisy_observation_feature <- function(
+  feature, 
+  n_sample, 
+  epsilon 
+){
+  real_features <- rep(feature, n_sample)
+  noisy <- rbernoulli(p = epsilon, n = n_sample)
+  return(ifelse(noisy, as.logical(1-real_features), real_features))
+  
+}
+
+
+noisy_observation_creature <- function(
+  creature, 
+  n_sample = 1, 
+  epsilon
+){
+  sapply(creature, function(y){noisy_observation_feature(
+    feature = y, 
+    n_sample = n_sample, 
+    epsilon = epsilon
+  )})
+  
+}
+
+generate_creature_sequence <- function(
+  block_length, 
+  deviant_positions, # takes a vector, 
+  total_feature, 
+  feature_theta, 
+  feature_number, 
+  dissimilar_ratio
+){
+  
+  
+  background_theta <- make_creature(total_feature = total_feature, 
+                                    feature_theta = feature_theta, 
+                                    feature_number = feature_number)
+  
+  
+  deviant_theta <- make_dissimilar_creature(creature = background_theta, 
+                                            dissimilar_ratio = dissimilar_ratio)
+  
+  
+  background <- make_individual_creature(background_theta)
+  deviant <- make_individual_creature(deviant_theta)
+  
+  block_list <- replicate(block_length, background, simplify = FALSE)
+  
+  if (length(deviant_positions) > 0){
+    block_list[deviant_positions] <- replicate(length(deviant_positions),
+                                               deviant,
+                                               simplify = FALSE)
+  }
+  
+  # putting everything in a tibble dataframe  
+  tidy_creature_sequence <- bind_rows(lapply(block_list,
+                                             function(x) x %>% as_tibble_row(.name_repair = make.names))) 
+  
+  # renaming columns 
+  rename_column <- function(x){paste0("V", x)}
+  tidy_column_names <- lapply(1:total_feature, rename_column)
+  colnames(tidy_creature_sequence) <- tidy_column_names
+  
+  tidy_creature_sequence <- tidy_creature_sequence %>% 
+    mutate(trial_number = row_number(), 
+           trial_type = case_when(
+             trial_number %in% deviant_positions ~ "deviant", 
+             TRUE ~ "background"
+           ))
+  
+  
+  return(tidy_creature_sequence)
+  
+}
+
---
+++ tests/cluster_speed_test/forced_helper/make_scheme_and_params.r
@@ -0,0 +1,184 @@
+
+
+forced_set_model_params <- function(alpha_priors, 
+                             beta_priors, 
+                             alpha_epsilons, 
+                             beta_epsilosn, 
+                             noise_parameters, 
+                             world_EIGs, 
+                             max_observation, 
+                             forced_exposure_n){
+  
+  model_params_df <- crossing(
+    alpha_prior = alpha_priors,
+    beta_prior = beta_priors, 
+    alpha_epsilon = alpha_epsilons, 
+    beta_epsilon = beta_epsilosn,
+    noise_parameter = noise_parameters,
+    world_EIG = world_EIGs, 
+    max_observation = max_observation, 
+    forced_exposure_n = forced_exposure_n
+  ) %>% 
+    mutate(params_info = paste("ae", alpha_epsilon, 
+                               "be", beta_epsilon, 
+                               "ap", alpha_prior, 
+                               "bp", beta_prior, 
+                               "np", noise_parameter, 
+                               "wEIG", world_EIG, 
+                               "fexp", forced_exposure_n, 
+                               sep = "_"),
+           params_id = row_number())
+  
+  return(model_params_df)
+  
+}
+
+set_stim_params <- function(sequence_scheme, features_df){
+  
+  
+  
+  stimulus_sequence_df <- expand_grid(sequence_scheme, features_df)%>% 
+    rowwise() %>% 
+    mutate(
+    #  total_features_n = features_df$total_features_n, 
+   #   on_features_n = features_df$on_features_n, 
+      stimuli_sequence = nest(scheme_to_stimuli(sequence_scheme, 
+                                                n_features = n_features, 
+                                                on_features_n = on_features_n), data = everything())) %>% 
+    ungroup() %>% 
+    mutate(stim_info = paste("nf", n_features, 
+                             "of", on_features_n, 
+                             "ss", sequence_scheme, 
+                             sep = "_"))
+
+  
+  return(stimulus_sequence_df)
+}
+
+set_stim_params_by_block <- function(sequence_scheme, features_df){
+  
+  
+  
+  stimulus_sequence_df <- expand_grid(sequence_scheme, features_df)%>% 
+    rowwise() %>% 
+    mutate(
+      block_length = nchar(sequence_scheme),
+      stimuli_sequence = nest(scheme_to_stimuli(sequence_scheme, 
+                                                n_features = n_features, 
+                                                on_features_n = on_features_n), data = everything())) %>% 
+    ungroup() %>% 
+    mutate(stim_info = paste("nf", n_features, 
+                             "of", on_features_n, 
+                             "ss", sequence_scheme, 
+                             sep = "_"),
+           block_number = row_number()) 
+  
+  block_info <- tibble(
+    sequence_scheme = paste(stimulus_sequence_df$sequence_scheme, collapse = "")
+  )
+  
+  # now extend it into a block 
+
+  
+  block_stimuli_sequence <- block_info %>% 
+    mutate(
+      stimuli_sequence = nest(lapply(seq(1, length(stimulus_sequence_df$stimuli_sequence$data)), 
+         function(i){stimulus_sequence_df$stimuli_sequence$data[[i]] %>% 
+             mutate(block_number = i)}) %>% 
+    bind_rows() %>% 
+    mutate(trial_number_in_block = trial_number,
+           trial_number = row_number()), 
+    data = everything()), 
+    n_features = stimulus_sequence_df$n_features[[1]]
+    )
+  
+  
+  
+  return(block_stimuli_sequence)
+}
+
+
+
+
+make_simulation_params <- function(n_sim,
+                                   model_params, 
+                                   stim_params
+){
+  
+  expand_grid(model_params, stim_params) %>% 
+    left_join(expand_grid(sub_id = 1:n_sim, params_id = model_params$params_id))
+  
+}
+
+
+make_simulation_params_block <- function(n_sim,
+                                   model_params, 
+                                   stim_params
+){
+  
+  model_params %>% 
+    mutate(stimuli_sequence = nest(stim_params)) %>% 
+    
+  
+  expand_grid(model_params, stim_params) %>% 
+    left_join(expand_grid(sub_id = 1:n_sim, params_id = model_params$params_id))
+  
+}
+
+
+
+scheme_to_stimuli <- function(sequence_scheme, n_features, on_features_n){
+  
+  
+  background_creature <- sample(c(rep(FALSE, n_features - on_features_n), 
+                                  rep(TRUE, on_features_n)))
+  
+  # construct deviant creature from background creature 
+  deviant_creature <- background_creature
+  
+  # if not all features can be flipped, then select a random subset of the features to flip as many as possible 
+  if (on_features_n > n_features * .5){
+    # randomly select a subset of features to be flipped 
+    # R has annoying behaviors for sample when the vector length is 1
+    # can't use if_else because it has an annoying behavior that forces both clause return the same thing 
+    
+    if (n_features - on_features_n == 1 | n_features == on_features_n){
+
+      on_location <-  sample(which(background_creature == TRUE), 1)
+      off_location <-  which(background_creature == FALSE)
+    }else{
+      on_location <- sample(which(background_creature == TRUE), size = n_features - on_features_n)
+      off_location <- sample(which(background_creature == FALSE), size = n_features - on_features_n)
+    }
+  
+  # if all features can be flipped, flip all features   
+  }else{
+    on_location <- which(background_creature == TRUE)
+    off_location <- sample(x = which(background_creature == FALSE), size = on_features_n)
+  }
+  
+  deviant_creature <- background_creature
+  deviant_creature[on_location] <- FALSE 
+  deviant_creature[off_location] <- TRUE 
+  
+  block_list <- strsplit(sequence_scheme, "")[[1]]
+  
+  background_sequence <- replicate(length(block_list), background_creature, simplify = FALSE)
+  background_sequence[which(block_list == "D")] <- replicate(length(which(block_list == "D")), 
+                                                             deviant_creature, simplify = FALSE)
+  
+  tidy_creature_sequence <- bind_rows(lapply(background_sequence,
+                                             function(x) x %>% as_tibble_row(.name_repair = make.names))) 
+  
+  # renaming columns 
+  rename_column <- function(x){paste0("V", x)}
+  tidy_column_names <- lapply(1:length(background_creature), rename_column)
+  colnames(tidy_creature_sequence) <- tidy_column_names
+  tidy_creature_sequence <- tidy_creature_sequence %>% 
+    mutate(trial_number = row_number())
+  
+  return(tidy_creature_sequence)
+  
+}
+
+
