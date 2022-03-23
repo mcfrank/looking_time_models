67260ca007e037272d17e1f99cc7ab6cacae0aa7
GalRaz <galraz@gmail.com>
2022-03-23 18:26:17 -0400

    add eucledian and cosine distances

---
+++ embeddings/embedding_distances.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+import json
+from scipy.spatial import distance_matrix
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+
+f = open('embeddings_unity.json')
+embedding_dict = json.load(f)
+
+# samples in rows, features in columns
+embedding_df = pd.DataFrame.from_dict(embedding_dict).transpose() 
+
+# calculate cosine distance
+cosine_distances = 1 - cosine_similarity(embedding_df.values, embedding_df.values)
+cosine_distances_df = pd.DataFrame(cosine_distances, index=embedding_df.index, columns=embedding_df.index)
+
+cosine_distances_df.to_csv('cosine_distances.csv')
+
+# calculate eucledian distance
+embedding_eucledian_distances = distance_matrix(embedding_df.values, embedding_df.values)
+eucledian_distances_df = pd.DataFrame(embedding_eucledian_distances, index=embedding_df.index, columns=embedding_df.index)
+
+eucledian_distances_df.to_csv('eucledian_distances.csv')
