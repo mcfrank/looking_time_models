55ef2dba967e840aea5cbed505892d20bca74ff6
anjie cao <anjiecao@stanford.edu>
2022-04-16 18:57:29 -0700

    some progress in precalculating prior df

--- gaussian_ranch/helper/granch_sim_setup_helper.R
+++ gaussian_ranch/helper/granch_sim_setup_helper.R
@@ -1,5 +1,7 @@
 
-set_granch_params <- function(mu_priors, 
+set_granch_params <- function(
+                            grid_mu_theta, grid_sig_sq, grid_y,grid_epsilon,
+                            mu_priors, 
                              V_priors, 
                              alpha_priors, 
                              beta_priors, 
@@ -33,6 +35,16 @@ set_granch_params <- function(mu_priors,
                                sep = "_"),
            params_id = row_number())
   
+  
+  model_params_df <- model_params_df %>% 
+    mutate(grid_mu_theta = list(grid_mu_theta), 
+           grid_sig_sq = list(grid_sig_sq), 
+           grid_y = list(grid_y), 
+           grid_epsilon = list(grid_epsilon)) %>% 
+    select(grid_mu_theta, grid_sig_sq, grid_y, grid_epsilon, mu_prior, V_prior, 
+           alpha_prior, beta_prior, epsilon, mu_epsilon, sd_epsilon, world_EIG, max_observation)
+  
+  
   return(model_params_df)
   
 }
@@ -50,4 +62,49 @@ make_simulation_params <- function(n_sim,
     left_join(expand_grid(sub_id = 1:n_sim, params_id = model_params$params_id))
   
 }
+
+
+
+# get the prior df so that we don't need to calculate multiple times throughout
+make_lp_mu_sig_sq <- function(grid_mu_theta, grid_sig_sq,
+                          mu_prior, lambda_prior, alpha_prior, beta_prior){
+  
+  lp_mu_sig_sq <- expand.grid(grid_mu_theta = grid_mu_theta,
+                              grid_sig_sq = grid_sig_sq)
+  lp_mu_sig_sq$lp_mu_sig_sq = mapply(score_mu_sig_sq, 
+                                     lp_mu_sig_sq$grid_mu_theta, lp_mu_sig_sq$grid_sig_sq, 
+                                     mu = mu_prior, 
+                                     lambda = lambda_prior, 
+                                     alpha = alpha_prior, 
+                                     beta = beta_prior, log = TRUE)
+  
+  return(lp_mu_sig_sq)
+}
+
+make_df_y_given_mu_sig_sq <- function(lp_mu_sig_sq, grid_y){
+  get_df_y_given_mu_sig_sq(lp_mu_sig_sq, grid_y)
+}
+
+
+ 
+
+make_prior_df <- function(lp_mu_sig_sq, grid_epsilon, prior_mu_epsilon, prior_sd_epsilon){
+  # needs to add lp_epsilon here 
+  lp_epsilon = tibble(grid_epsilon = grid_epsilon)
+  lp_epsilon$lp_epsilon = mapply(score_epsilon, 
+                                 lp_epsilon$grid_epsilon, mu = prior_mu_epsilon, sd = prior_sd_epsilon)
+  
+  prior_df <- merge(lp_mu_sig_sq, lp_epsilon)
+  return(prior_df)
+  
+}
+  
+
+
+
+
+
+
+
+
 
