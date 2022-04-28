187a6cebbfcb314b71e622998e057beb1106a10c
anjie cao <anjiecao@stanford.edu>
2022-04-28 10:55:49 -0700

    fixed posterior integration bug

--- gaussian_ranch/granch_explorations.R
+++ gaussian_ranch/granch_explorations.R
@@ -10,6 +10,23 @@ ll_z_given_mu_sig_sq[[1]][[1]] |>
   geom_point() 
 
 
+six_this_lp_z_given_mu_sig_sq |>
+  group_by(grid_mu_theta) |>
+  summarise(lp_z_given_mu_sig_sq = matrixStats::logSumExp(lp_z_given_mu_sig_sq)) |>
+  ggplot(aes(x = grid_mu_theta, y = lp_z_given_mu_sig_sq)) + 
+  geom_point() 
+
+seven_this_lp_z_given_mu_sig_sq |>
+  group_by(grid_mu_theta) |>
+  summarise(lp_z_given_mu_sig_sq = matrixStats::logSumExp(lp_z_given_mu_sig_sq)) |>
+  ggplot(aes(x = grid_mu_theta, y = lp_z_given_mu_sig_sq)) + 
+  geom_point() 
+
+real_seven_lp_z_given_mu_sig_sq |>
+  group_by(grid_mu_theta) |>
+  summarise(lp_z_given_mu_sig_sq = matrixStats::logSumExp(lp_z_given_mu_sig_sq)) |>
+  ggplot(aes(x = grid_mu_theta, y = lp_z_given_mu_sig_sq)) + 
+  geom_point() 
 # check posterior
 
 posts <- map(ll_post, function(x) x[[1]])
@@ -26,5 +43,5 @@ all_posts |>
   geom_point() + 
   facet_wrap(~idx)
 
-max(ll_post[[1]][[1]]$posterior)
-max(ll_post[[2]][[1]]$posterior)
+max(ll_post[[8]][[1]]$posterior)
+max(ll_post[[7]][[1]]$posterior)
