b7b3cf368fc0daec7240a9b0183b990be0dbe4e2
anjie cao <anjiecao@stanford.edu>
2022-05-13 20:42:57 -0700

    granch cluster

---
+++ gaussian_ranch/cluster_granch/helper/init.r
@@ -0,0 +1,85 @@
+
+initialize_post_df <- function(prior_df, max_observation, feature_number){
+  
+  df <- prior_df 
+  df$lp_z_given_mu_sig_sq <- rep(NA_real_, nrow(df))
+  df$unnormalized_log_posterior <- rep(NA_real_, nrow(df))
+  df$log_posterior <- rep(NA_real_, nrow(df))
+  df$posterior <- rep(NA_real_, nrow(df))
+  
+  ll_post_df <- lapply(seq(1, max_observation, 1), 
+                       function(x){
+                         lapply(seq(1, feature_number, 1), 
+                                function(y){
+                                  df
+                                })
+                       })
+  return(ll_post_df)
+  
+}
+
+
+initialize_z_given_mu_sig_sq <- function(prior_df, max_observation, feature_number){
+  
+  df <- prior_df
+  df$lp_z_bar_given_all_ys_mu_sig_sq <- rep(NA_real_, nrow(df))
+  
+  z_given_mu_sig_sq <- lapply(seq(1, max_observation, 1), 
+                          function(x){
+                            lapply(seq(1, feature_number, 1), 
+                                   function(y){
+                                     df
+                                   })
+                          })
+  return(z_given_mu_sig_sq)
+}
+
+
+noisy_observation <- function(stimulus, epsilon){
+  sapply(stimulus, function(x){rnorm(1, mean = x, sd = epsilon)}) 
+}
+
+initialize_model <- function(eig_from_world, max_observation, n_features, measurement = "EIG") {
+  
+  model <- tibble(t = rep(NA,max_observation),
+                  stimulus_idx = rep(NA,max_observation), 
+                  EIG_from_world = rep(eig_from_world,max_observation),
+                  p_look_away = rep(NA,max_observation), 
+                  look_away = rep(NA,max_observation)) 
+  
+  if(measurement == "EIG"){
+    model$EIG = rep(NA,max_observation)
+  }else if(measurement == "KL"){
+    model$KL = rep(NA,max_observation)
+  }else if(measurement == "surprisal"){
+    model$surprisal = rep(NA,max_observation)
+  }
+  
+  # initialize columns for observations
+  model[,paste("f", 1:n_features, sep="")] <- NA
+  
+  return(model)
+}
+
+
+
+get_all_possible_observations_for_stimulus <- function(stimulus, epsilon, grid_n){
+  lapply(stimulus, function(y){seq(y - epsilon *5, y + epsilon *5, epsilon * 10 / (grid_n - 1))}) %>% 
+    expand.grid() %>% 
+    rename_with(~gsub("Var", "V", .x))
+}
+
+
+
+initialize_model_testing_infrastructrue <- function(max_observation){
+  ll_model_testing <- lapply(seq(1, max_observation, 1), 
+                             function(x){
+                               lapply(seq(1, 4, 1), 
+                                      function(y){
+                                          NULL
+                                        })
+                                      
+                             })
+  
+  return(ll_model_testing)
+}
--- gaussian_ranch/granch_explorations.R
+++ gaussian_ranch/granch_explorations.R
@@ -1,10 +1,40 @@
-# check EIG 
+# understand why the bumps
+library(tidyverse)
+library(here)
 
+res <- readRDS(here("first_embeddings.RDS"))
+
+model <- res$results[[2]]
+model  %>% 
+  group_by(stimulus_idx) %>% 
+  filter(!is.na(stimulus_idx)) %>% 
+  summarise(sample_n = n()) %>% 
+  ggplot(aes(x = stimulus_idx, y = sample_n)) + 
+  stat_summary(position = position_dodge(width = .1),fun.data = "mean_cl_boot") 
+
+model  %>% 
+  ggplot(aes(x = t, y = EIG, color = as.factor(stimulus_idx))) + 
+  stat_summary(position = position_dodge(width = .1),fun.data = "mean_cl_boot") 
 all_sims_res$results[[2]] %>% 
-  filter(stimulus_idx == 3)
+
+  
+model %>% 
+  filter(stimulus_idx == 4)
+
+# t = 89 where the first observation on stimulus 4
+# t = 88 the last observaton on stimulu 3
+
+# 3 -> 4
+model %>% 
+  filter(t == 88 | t == 89)
+# 2 -> 3
+model %>% 
+  filter(t == 60 | t == 61)
 
 
-
+l_posteriors <-  res$results[[1]][[1]]
+ 
+l_posteriors[[88]]
 
 # check likelihood
 
