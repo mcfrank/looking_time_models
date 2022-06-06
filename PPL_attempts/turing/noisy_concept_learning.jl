58c7475d69d3c7b31e4ef01b68cbbfc54af67f2d
GalRaz <galraz@gmail.com>
2022-06-05 23:21:35 -0400

    ppl stuff

--- PPL_attempts/turing/noisy_concept_learning.jl
+++ PPL_attempts/turing/noisy_concept_learning.jl
@@ -1,5 +1,5 @@
 # Import libraries.
-using Turing, DynamicHMC, StatsPlots, Random, Distributions, StatsBase, LinearAlgebra
+using Turing, DynamicHMC, StatsPlots, Random, Distributions, StatsBase, LinearAlgebra, FillArrays, TruncatedDistributions
 
 # Set the priors on mean and SD of the concept
 
@@ -12,15 +12,15 @@ using Turing, DynamicHMC, StatsPlots, Random, Distributions, StatsBase, LinearAl
 σ_beta = 1
 
 # noise SD
-ϵ = 0.5
+noise = 0.5
 
 # Set up data from a Bernoulli distribution, i.e. draw heads or tails.
 num_samples = 100
 num_exemplars = 5
-num_features = 2
+num_features = 3
 
 # Random initialization
-Random.seed!(1)
+Random.seed!(12)
 idx_weights = fill(1/num_exemplars, num_exemplars)
 
 # generate Data
@@ -44,7 +44,7 @@ model_input = Dict(:μ_mean => μ_mean,
                    :μ_sd => μ_sd,
                    :σ_alpha => σ_alpha,
                    :σ_beta => σ_beta,
-                   :ϵ => ϵ,
+                   :noise => noise,
                    :num_samples=>num_samples, 
                    :num_exemplars=>num_exemplars, 
                    :num_features=>num_features, 
@@ -58,25 +58,25 @@ model_input = Dict(:μ_mean => μ_mean,
     μ = Vector{Float64}(undef, model_input[:num_features])
     σ = Vector{Float64}(undef, model_input[:num_features])
     
-    # loop through feautures
-    for f in 1:model_input[:num_features]
+    # set mu & sigma
+    μ ~ MvNormal(Fill(model_input[:μ_mean], num_features), model_input[:μ_sd] * I)
 
-        μ[f] ~ Normal(model_input[:μ_mean], model_input[:μ_sd])
-        σ[f] ~ Gamma(model_input[:σ_alpha],model_input[:σ_beta])
-
-        y = Matrix{Float64}(undef, model_input[:num_features], model_input[:num_exemplars])
-
-        # loop through y's
-        for i in 1:model_input[:num_exemplars]
-            y[f, i] ~ Normal(μ[f], σ[f])
-        end
-        
-        # loop through z's
-        for i in 1:model_input[:num_samples]
-            model_input[:data][i][f] ~ Normal(y[model_input[:cats][i]], model_input[:ϵ])
-        end
-
+    for i in 1:model_input[:num_features]
+        σ[i] ~ Gamma(model_input[:σ_alpha], model_input[:σ_beta])
     end
+
+    y = Vector{Float64}(undef, model_input[:num_exemplars])
+
+    # loop through y's
+    for i in 1:model_input[:num_exemplars]
+        y[i] ~ MvNormal(μ, 2)
+    end
+    
+    # loop through z's
+    for i in 1:model_input[:num_samples]
+        model_input[:data][i] ~ MvNormal(y[model_input[:cats][i]], model_input[:noise])
+    end
+
     
     return μ, σ
 end
@@ -86,8 +86,12 @@ iterations = 10000
 ϵ = 0.05
 τ = 10
 
+
 # Start sampling.
-chain = sample(noisy_concept_learning(model_input), DynamicNUTS(), iterations)
+@time chain = sample(noisy_concept_learning(model_input), DynamicNUTS(), iterations)
+
 
 # Plot a summary of the sampling process for the parameter p, i.e. the probability of heads in a coin.
 p = plot(chain)
+
+savefig(p,"file.png")
