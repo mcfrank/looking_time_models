2fbd77c7edf2984fe83ebf1b5f7c37e90822631f
anjie cao <anjiecao@stanford.edu>
2022-09-29 16:10:26 -0700

    get interface to read in the embeddings + create real sequence

--- GRANCH_python/foo.py
+++ GRANCH_python/foo.py
@@ -4,7 +4,30 @@ import numpy as np
 import pandas as pd
 import helper
 import compute_prob
-a = torch.tensor([-1279.1746])
+import warnings
 
-print(torch.exp())
-print(torch.exp(a).item() == 0)
+
+a = pd.read_csv("embedding_PCA.csv", header = None)
+print(a)
+
+
+print(a.sample(2).iloc[:,0:3])
+
+bd_pair = a.sample(2).iloc[:,0:3]
+
+b = torch.tensor(bd_pair.iloc[0, :])
+d = torch.tensor(bd_pair.iloc[1, :])
+
+idx = 0
+stimuli_sequence = {}
+sequence_scheme = "BBDBBBBB"
+n_trial = len(sequence_scheme)
+while idx < n_trial: 
+    if(sequence_scheme[idx] == "B"): 
+        stimuli_sequence[idx] = b
+    elif(sequence_scheme[idx] == "D"): 
+        stimuli_sequence[idx] = d
+    else: 
+        warnings.warn("Wrong sequence scheme ")
+    idx = idx + 1
+print(stimuli_sequence)
