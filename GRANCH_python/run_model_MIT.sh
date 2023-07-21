818ff255fc7aef2bf4152978379401dbebc0acc3
GalRaz <galraz@gmail.com>
2023-07-21 15:24:57 -0400

    slight modifications + mit cluster script

---
+++ GRANCH_python/run_model_MIT.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --mail-type=END
+#SBATCH -n 1 
+#SBATCH --mem=16GB
+#SBATCH --constraint=16GB
+#SBATCH --gres=gpu:1
+#SBATCH --time=10:00:00
+#SBATCH --output=R-%x.%j.out
+#SBATCH --error=R-%x.%j.err
+
+cmd="python3 cluster_granch.py"
+
+echo $cmd 
+
+$cmd
+
