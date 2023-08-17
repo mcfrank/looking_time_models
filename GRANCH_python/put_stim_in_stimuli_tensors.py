e4000b3994784d16603e2a24cd32152696963dc3
GalRaz <galraz@gmail.com>
2023-08-16 20:59:49 -0400

    oops

--- GRANCH_python/put_stim_in_stimuli_tensors.py
+++ GRANCH_python/put_stim_in_stimuli_tensors.py
@@ -1,7 +1,7 @@
 import torch
 from granch_utils import num_stab_help
 
-stimuli_info_list = num_stab_help.sample_condition_experiment(20)
+stimuli_info_list = num_stab_help.sample_condition_experiment(5)
 
 for idx, f in enumerate(stimuli_info_list):
     torch.save(f, "stimuli_tensors/"+str(idx)+"stim.pt")
