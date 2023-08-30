445a21b192812217d69abed4caa8ef8854289fac
Gal Raz <galraz@Gals-MacBook-Pro.local>
2023-08-29 23:59:03 -0400

    modify summarize

--- GRANCH_python_babies/summarize/summarize.py
+++ GRANCH_python_babies/summarize/summarize.py
@@ -2,11 +2,19 @@ import os
 import pandas as pd
 import ipdb 
 import uuid
+import datetime
 
 folder_path = "../cache_results/"
 df_list = []
 
-for idx, file_name in enumerate(os.listdir(folder_path)):
+file_list = os.listdir(folder_path)
+
+# only get recent files
+cut_off_date = datetime.datetime(2023, 8, 26, 22, 19, 31, 882493)
+timestamp_cutoff = datetime.datetime.timestamp(cut_off_date)
+newer_files = [f for f in file_list if os.path.getmtime(os.path.join(folder_path,f)) >= timestamp_cutoff]
+
+for idx, file_name in enumerate(file_list[::-1]):
     pattern_batch_info = r"batch_(\d+)_cache_([A-Z]+)"
     #pattern_stim_spec = r"b_([\d\.]+)_d_([\d\.]+)"
     if file_name.endswith(".pickle"):
@@ -14,6 +22,7 @@ for idx, file_name in enumerate(os.listdir(folder_path)):
         try:
             df = pd.read_pickle(file_path)
             df["batch_id"] = idx
+            df["file_name"] = file_name
             df_list.append(df)
             print(pd.unique(df['stim_squence']))
         except: 
@@ -26,11 +35,6 @@ for idx, file_name in enumerate(os.listdir(folder_path)):
                                                                                                              'mu_prior': 'first','v_prior': 'first','alpha_prior': 'first','beta_prior': 'first',
                                                                                                              'epsilon': 'first'}).reset_index()
         counts["n_sample"] =  main_df.groupby(['batch_id', 'j_i',  "stimulus_id", "stim_squence", "violation_type"], as_index=False).count()['EIG']
-        counts.to_csv("summarized_results_detailed.csv", mode = 'a', index=False, header=False)
+        counts.to_csv("summarized_results_detailed_new_files.csv", mode = 'a', index=False, header=False)
         df_list = []
 
-counts = pd.read_csv("summarized_results_detailed.csv")
-summary = counts.groupby(["stim_squence", "violation_type", 'stimulus_id', 'epsilon'], as_index = False).mean()
-combined_df = summary[["stim_squence", "violation_type", "stimulus_id", "n_sample", "epsilon"]]
-
-combined_df.to_csv("summarized_results_coarse.csv")
