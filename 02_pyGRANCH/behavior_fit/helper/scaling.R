e9a0275cdac583e7ece4ca573a5b887066af3aa4
anjie cao <anjiecao@stanford.edu>
2023-09-29 11:30:19 -0700

    plotting info

--- 02_pyGRANCH/behavior_fit/helper/scaling.R
+++ 02_pyGRANCH/behavior_fit/helper/scaling.R
@@ -15,3 +15,37 @@ get_scaling_param <- function(all_sim_d){
   return(all_sim_d)
   
 }
+
+
+get_scaling_param_constraint <- function(all_sim_d){
+  
+  all_sim_d$intercept <- unlist(map(all_sim_d$data, function(x){
+    intercept = colf::colf_nlxb(mean_sample ~ mean_lt, d = x, lower = c(-Inf, 0.0000471))$coefficients[[1]]
+    return(intercept)
+  })) 
+  
+  all_sim_d$coef <- unlist(map(all_sim_d$data, function(x){
+    coef = colf::colf_nlxb(mean_sample ~ mean_lt, d = x, lower = c(-Inf, 0.0000471))$coefficients[[2]]
+    return(coef)
+  })) 
+  
+  return(all_sim_d)
+  
+}
+
+
+get_scaling_param_flipped <- function(all_sim_d){
+  
+  all_sim_d$intercept <- unlist(map(all_sim_d$data, function(x){
+    intercept = colf::colf_nlxb(mean_lt ~ mean_sample, d = x, lower = c(-Inf, 0))$coefficients[[1]]
+    return(intercept)
+  })) 
+  
+  all_sim_d$coef <- unlist(map(all_sim_d$data, function(x){
+    coef = colf::colf_nlxb(mean_lt ~ mean_sample, d = x, lower = c(-Inf, 0))$coefficients[[2]]
+    return(coef)
+  })) 
+  
+  return(all_sim_d)
+  
+}
