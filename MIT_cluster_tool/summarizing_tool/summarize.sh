973db1c0bbf62665778cf6759ddf4bb6f82053ce
anjie cao <anjiecao@stanford.edu>
2023-11-02 20:24:25 -0700

    replace multi_cluster leisioned sim

--- MIT_cluster_tool/summarizing_tool/summarize.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -l
-#SBATCH --mail-type=END
-#SBATCH --mail-type=BEGIN
-#SBATCH --mem=128GB
-#SBATCH --output=summarize.%A_%a.out
-#SBATCH --time=10:00:00
-
-python -m 02_pyGRANCH.MIT_cluster_tool.summarizing_tool.summarize
